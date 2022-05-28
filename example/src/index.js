ab785253550b33d4e0b27a6c0d652ac6fe0bcf6b
Shyam Gupta <shyamsundar.g@kiprosh.com>
2022-05-28 18:08:18 +0530

    Cleanup

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
