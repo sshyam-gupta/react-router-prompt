ffa9f1aa00576326547c6a02d96f9e86815d22c6
oussama <oussama2@cominity.ca>
2022-10-26 10:12:20 +0100

    ci(test): migrate test to vitest

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import ReactRouterPrompt from "../src";
+import {describe, it} from 'vitest'
 
 describe("it", () => {
   it("renders without crashing", () => {
