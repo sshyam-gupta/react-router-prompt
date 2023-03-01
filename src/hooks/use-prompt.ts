18595b62a02697f9c1975bc47797bc46a0cc1b1f
Rudel Vakilov <54040802+RudikS-git@users.noreply.github.com>
2023-03-02 00:23:43 +0400

    "when" can be function
    I ran into this problem when using "when" as function 'cause the function is always defined

--- src/hooks/use-prompt.ts
+++ src/hooks/use-prompt.ts
@@ -18,24 +18,25 @@ import {
 // out of whack. You should test your own implementation thoroughly to make sure
 // the tradeoffs are right for your users.
 function usePrompt(when: boolean | BlockerFunction): Blocker {
-  const blocker = useBlocker(when)
+  const _when = typeof when === 'function'? when() : when;
+  const blocker = useBlocker(_when)
   useEffect(() => {
     // Reset if when is updated to false
-    if (blocker.state === "blocked" && !when) {
+    if (blocker.state === "blocked" && !_when) {
       blocker.reset()
     }
-  }, [blocker, when])
+  }, [blocker, _when])
 
   useBeforeUnload(
     useCallback(
       (event) => {
-        if (when) {
+        if (_when) {
           event.preventDefault()
           // eslint-disable-next-line no-param-reassign
           event.returnValue = "Changes that you made may not be saved."
         }
       },
-      [when],
+      [_when],
     ),
     { capture: true },
   )
